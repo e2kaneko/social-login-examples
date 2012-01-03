a44c6538144043b1016eaf38c6bba92f670550d0
e2kaneko <e2kaneko@gmail.com>
2012-01-03 22:36:07 +0900

    ダミーページ・パンくずリスト追加

---
+++ app/Controller/LoginController.php
@@ -0,0 +1,13 @@
+<?php
+class LoginController extends AppController {
+	public $name = 'Login';
+	public $helpers = array('Html', 'Form');
+	public $components = array('Session');
+
+	public function facebook() {
+	}
+
+	public function twitter() {
+	}
+}
+
---
+++ app/View/Login/facebook.ctp
@@ -0,0 +1,10 @@
+<ul class="breadcrumb">
+  <li><?php echo $this->Html->link("Home", array('action' => '../List'));?> <span class="divider">/</span></li>
+  <li class="active">Facebook Login</li>
+</ul>
+
+<?php
+
+echo 'fblogin';
+
+?>
