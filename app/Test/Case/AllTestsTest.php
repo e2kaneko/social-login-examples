9224128f184f2893ab60680b62a0adaffd29434e
e2kaneko <e2kaneko@gmail.com>
2012-01-26 23:30:09 +0900

    test

---
+++ app/Test/Case/AllTestsTest.php
@@ -0,0 +1,17 @@
+<?php
+class AllTests extends PHPUnit_Framework_TestSuite {
+
+/**
+ * Suite define the tests for this suite
+ *
+ * @return void
+ */
+	public static function suite() {
+		$suite = new PHPUnit_Framework_TestSuite('All Tests');
+
+		$path = APP_TEST_CASES . DS;
+
+		$suite->addTestFile($path . 'AllControllerTest.php');
+		return $suite;
+	}
+}
