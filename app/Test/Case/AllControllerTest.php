91ab54b547946a9bcf4cdfe7864d470bb3304336
e2kaneko <e2kaneko@gmail.com>
2012-01-28 22:34:47 +0900

    テスト追加

--- app/Test/Case/AllControllerTest.php
+++ app/Test/Case/AllControllerTest.php
@@ -10,6 +10,7 @@ class AllControllersTest extends PHPUnit_Framework_TestSuite {
 		$suite = new CakeTestSuite('All Controller related class tests');
 
 		$suite->addTestFile(APP_TEST_CASES . DS . 'Controller' . DS . 'ListControllerTest.php');
+		$suite->addTestFile(APP_TEST_CASES . DS . 'Controller' . DS . 'GithubControllerTest.php');
 		$suite->addTestFile(APP_TEST_CASES . DS . 'Controller' . DS . 'TwitterControllerTest.php');
 		$suite->addTestFile(APP_TEST_CASES . DS . 'Controller' . DS . 'FacebookControllerTest.php');
 		return $suite;
