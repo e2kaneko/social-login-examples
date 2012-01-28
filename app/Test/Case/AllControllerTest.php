954a8e83210a0abb39750f9aafb11dd03242aef6
e2kaneko <e2kaneko@gmail.com>
2012-01-28 22:21:15 +0900

    新規追加

--- app/Test/Case/AllControllerTest.php
+++ app/Test/Case/AllControllerTest.php
@@ -1,15 +1,17 @@
 <?php
 class AllControllersTest extends PHPUnit_Framework_TestSuite {
 
-/**
- * suite method, defines tests for this suite.
- *
- * @return void
- */
+	/**
+	 * suite method, defines tests for this suite.
+	 *
+	 * @return void
+	 */
 	public static function suite() {
 		$suite = new CakeTestSuite('All Controller related class tests');
 
 		$suite->addTestFile(APP_TEST_CASES . DS . 'Controller' . DS . 'ListControllerTest.php');
+		$suite->addTestFile(APP_TEST_CASES . DS . 'Controller' . DS . 'TwitterControllerTest.php');
+		$suite->addTestFile(APP_TEST_CASES . DS . 'Controller' . DS . 'FacebookControllerTest.php');
 		return $suite;
 	}
 }
